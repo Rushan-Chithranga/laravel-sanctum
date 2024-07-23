2d0863f7cb26e39ad39773c092a6eaa2cb45d5b8
rushan <rushan@thesanmark.com>
2024-07-23 16:53:58 +0530

    done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,47 +4,117 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
+use Throwable;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
+        try
+            {
+                $validateUser = Validator::make($request->all(),
+                [
+                    "name" => "required|string",
+                    "email" => "required|email|unique:users",
+                    "password" => "required|min:6"
+                ]);
 
-        $data = $request->validate([
-            "name" => "required|string",
-            "email" => "required|email|unique:users",
-            "password" => "required|min:6"
-        ]);
+                if ($validateUser->fails()) {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'validation failed',
+                        'errors' => $validateUser->errors()
+                    ],422);
+                }
 
-        $user= User::create($data);
-        $token = $user->createToken('auth_token')->plainTextToken;
+                $user= User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $request->password
+                ]);
+                $token = $user->createToken('auth_token', ['*'] , now()->addWeek())->plainTextToken;
 
-        return [
-            'user' => $user,
-            'token' => $token,
-            'messages' => 'User created successfully'
-        ];
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User Created successfully',
+                    'user' => $user,
+                    'token' => $token
+                ],201);
+                
+            } catch (\Throwable $th){
+
+                return response()->json([
+                    'status' => false,
+                    'message' => $th->getMessage()
+                ],419);
+            }
+
 
     }
     public function login(Request $request){
+        try
+        {
+            $validateUser = Validator::make($request->all(),
+        [
+                "email" => "required|email|exists:users",
+                "password" => "required|min:6"
+            ]);
 
-        $data = $request->validate([
-            "email" => "required|email|exists:users",
-            "password" => "required|min:6"
-        ]);
-        $user = User::where('email', $data['email'])->first();
-        if (!$user || !Hash::check($data['password'], $user->password)) {
-           return response([
-            'message' => 'Bad Creds'
-           ],401);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation failed',
+                    'errors' => $validateUser->errors()
+                ],422);
+            }
+            if (!Auth::attempt($request->only(['email','password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & password does not match with our record',
+                ],401);
+            }
+            $user = User::where('email',$request->email)->first();
+
+            $token = $user->createToken('auth_token',['*'] , now()->addWeek())->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged successfully',
+                'user' => $user,
+                'token' => $token
+            ],200);
+
+        } catch (\Throwable $th) {
+
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ],419);
         }
-        $token = $user->createToken('auth_token')->plainTextToken;
 
-        return [
-            'user' => $user,
-            'token' => $token,
-            'messages' => 'User Logged successfully'
-        ];
+    }
 
+    public function profile(){
+        $userData = auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile Information',
+            'data' => $userData,
+            'id' => auth()->user()->id
+        ],200);
+    }
+
+    public function logout(){
+        // auth()->user()->tokens()->delete();
+        PersonalAccessToken::where('tokenable_id', auth()->id())->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'UserLogout successfully',
+            'data' => [],
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post( "/register", [AuthController::class, "register"])->name("register");
 Route::post( "/login", [AuthController::class, "login"])->name("login");
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function(){
+    Route::get( "/profile", [AuthController::class, "profile"])->name("profile");
+    Route::get( "/logout", [AuthController::class, "logout"])->name("logout");
+});
